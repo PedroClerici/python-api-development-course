b8098d01c00fdfb56776845be9b455bd45d2671a
Pedro Clerici <pedro.clerici@tutamail.com>
2023-03-20 21:18:54 -0300

    Using pydantic for schema validations

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+# Creating a schema for request validation!
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = False
+    rating: Optional[int] = None
 
 
 # Handling get requests
@@ -12,5 +22,13 @@ def root():
 
 # Handling post requests
 @app.post("/")
-def create_post(body: dict = Body(...)):
-    return {key: value for key, value in body.items()}
+def get_post(payload: dict = Body(...)):
+    print(payload)
+    return {key: value for key, value in payload.items()}
+
+
+@app.post("/post")
+def create_post(payload: Post):
+    print(payload)
+    # Converting a pydantic model to a dict and returning it
+    return payload.dict()
